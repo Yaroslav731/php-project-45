0eb260a0ec946eda977d0695264373939fc5d50d
Ярослав Гончаров <yaroslav.x111@gmail.com>
2026-03-19 09:06:51 +0000

    style: fix PSR12 violastions in Cli.php

--- src/Cli.php
+++ src/Cli.php
@@ -7,20 +7,20 @@ use function cli\prompt;
 
 function greeting()
 {
-	line('Welcome to the Brain Games!');
-	line('May I have your name?');
-	
+    line('Welcome to the Brain Games!');
+    line('May I have your name?');
+
         $name = trim(fgets(STDIN));
 
-	sleep(1);
-	line("Привет, друзья! - {$name}!");
-
-        sleep(1);
-	line("Надеюсь у вас все - Слава Богу?)");
+    sleep(1);
+    line("Привет, друзья! - {$name}!");
 
         sleep(2);
-	line("Хочу пожелать вам благословеного дня! :)");
+    line("Надеюсь у вас все - Слава Богу?)");
 
         sleep(3);
-        line("Не забывайте про общение с Бoгом! а также... До встречи на домашечки в четверг! :)");
+    line("Хочу пожелать вам благословеного дня! :)");
+
+        sleep(3);
+        line("А также... До встречи на домашечки в четверг! :)");
 }
