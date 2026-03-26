5aef0ed06a95ba64deeb4ae2b334a918f56804d7
Ярослав Гончаров <yaroslav.x111@gmail.com>
2026-03-26 10:04:41 +0000

    Update greeting to return user name

--- src/Cli.php
+++ src/Cli.php
@@ -8,19 +8,7 @@ use function cli\prompt;
 function greeting()
 {
     line('Welcome to the Brain Games!');
-    line('May I have your name?');
-
-        $name = trim(fgets(STDIN));
-
-    sleep(1);
-    line("Привет, друзья! - {$name}!");
-
-        sleep(2);
-    line("Надеюсь у вас все - Слава Богу?)");
-
-        sleep(3);
-    line("Хочу пожелать вам благословеного дня! :)");
-
-        sleep(3);
-        line("А также... До встречи на домашечки в четверг! :)");
+    $name = prompt('May I have your name?');
+    line("Hello, {$name}!");
+    return $name;
 }
