0a5ea76f6a258d653cc29be039a082dcccc78d12
Ярослав Гончаров <yaroslav.x111@gmail.com>
2026-03-30 17:41:16 +0000

    feat: add brain-gcd game

---
+++ src/Gcd.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hexlet\Code;
+
+use function cli\line;
+use function cli\prompt;
+
+function findGcd(): void
+{
+        $description = 'Find the greatest common divisor of given numbers.';
+        $rounds = [];
+    for ($i = 0; $i < 3; $i++) {
+                $num1 = random_int(1, 50);
+                $num2 = random_int(1, 50);
+                $findGcd = gmp_gcd($num1, $num2);
+                $gcd = gmp_strval($findGcd);
+                $rounds[] = ['question' => $num1 . ' ' . $num2, 'answer' => $gcd];
+    }
+    runGame($description, $rounds);
+}
