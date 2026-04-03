a3a4b13a6a6eac00466586499089924228afb18f
Ярослав Гончаров <yaroslav.x111@gmail.com>
2026-04-03 08:02:10 +0000

    Update Prime.php

--- src/Games/Prime.php
+++ src/Games/Prime.php
@@ -14,7 +14,7 @@ function isPrime(int $n): bool
         return false;
     }
     for ($i = 5; $i * $i <= $n; $i += 6) {
-        if ($n % 1 == 0 || $n % ($i + 2) == 0) {
+        if ($n % $i == 0 || $n % ($i + 2) == 0) {
                  return false;
         }
     }
